710ddeee0c10f4a63238007177aaae5d79b04fc7
educationeliot1-max <educationeliot1@gmail.com>
2026-03-23 02:01:34 +0700

    🔥 Add next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,25 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static.wixstatic.com',
+      },
+    ],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['sharp'],
+  },
+};
+
+export default withNextIntl(nextConfig);
